20f2b727a531515d7ef25a1b2bf219ea92f067cf
Cesar Morales Garduño <shell.one.core@gmail.com>
2018-04-12 17:53:23 -0500

    Creación de fragmentos para Home, Arrivals y Departures. Implementación del comportamiento del clic en las opciones del menú principal.

--- app/src/main/java/com/example/shell/flyapplication/ui/MainActivity.kt
+++ app/src/main/java/com/example/shell/flyapplication/ui/MainActivity.kt
@@ -1,12 +1,16 @@
-package com.example.shell.flyapplication
-
+package com.example.shell.flyapplication.ui
 
 import android.os.Bundle
 import android.support.design.widget.NavigationView
+import android.support.v4.app.Fragment
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.widget.Toolbar
 import android.view.MenuItem
 import com.example.mylibrary.ToolbarActivity
+import com.example.shell.flyapplication.R
+import com.example.shell.flyapplication.ui.fragments.ArrivalsFragment
+import com.example.shell.flyapplication.ui.fragments.DeparturesFragment
+import com.example.shell.flyapplication.ui.fragments.HomeFragment
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : ToolbarActivity(), NavigationView.OnNavigationItemSelectedListener {
@@ -16,12 +20,17 @@ class MainActivity : ToolbarActivity(), NavigationView.OnNavigationItemSelectedL
         setContentView(R.layout.activity_main)
         loadToolbar(toolbar as Toolbar)
         setNavDrawer()
+        fragmentTransaction(HomeFragment())
+        navView.menu.getItem(0).isChecked = true
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         when(item.itemId) {
-
+            R.id.nav_home -> fragmentTransaction(HomeFragment())
+            R.id.nav_arrivals -> fragmentTransaction(ArrivalsFragment())
+            R.id.nav_departures -> fragmentTransaction(DeparturesFragment())
         }
+
         return true
     }
 
@@ -31,5 +40,11 @@ class MainActivity : ToolbarActivity(), NavigationView.OnNavigationItemSelectedL
         drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
         navView.setNavigationItemSelectedListener(this)
+    }
+
+    private fun fragmentTransaction(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+                .replace(R.id.container, fragment)
+                .commit()
     }
 }
